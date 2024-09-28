a8760a2e56782d18335620007fdfbc4137a553f0
iamsameed33 <sameed@digitify.com>
2024-09-28 11:24:27 +0500

    updated

---
+++ client/src/components/Sales.jsx
@@ -0,0 +1,204 @@
+import {
+  CategoryScale,
+  Chart,
+  Legend,
+  LinearScale,
+  LineElement,
+  Tooltip,
+  PointElement,
+  Title,
+} from "chart.js";
+import { Fragment, useEffect, useState } from "react";
+import { useQuery } from "@apollo/client";
+import { MONTHLY_SALES, WEEKLY_SALES } from "../query";
+import { format } from "date-fns";
+import { Line } from "react-chartjs-2";
+import {
+  startOfWeek,
+  endOfWeek,
+  eachDayOfInterval,
+  eachMonthOfInterval,
+  startOfYear,
+  endOfYear,
+} from "date-fns";
+import { DropdownIcon } from "../icons";
+
+Chart.register(
+  LineElement,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const Sales = () => {
+  const [activeData, setActiveData] = useState("weekly");
+  const [chartData, setChartData] = useState(null);
+  const [show, setShow] = useState(false);
+
+  const { data: weeklyData } = useQuery(WEEKLY_SALES);
+  const { data: monthlyData } = useQuery(MONTHLY_SALES);
+
+  useEffect(() => {
+    if (activeData === "weekly" && weeklyData) {
+      const currentWeekDays = eachDayOfInterval({
+        start: startOfWeek(new Date(), { weekStartsOn: 1 }),
+        end: endOfWeek(new Date(), { weekStartsOn: 1 }),
+      });
+
+      const mappedSalesData = currentWeekDays.map((day) => {
+        const saleForDay = weeklyData.weeklySales.find(
+          (sale) =>
+            format(new Date(sale.date), "yyyy-MM-dd") ===
+            format(day, "yyyy-MM-dd")
+        );
+        return saleForDay ? saleForDay.totalSales : 0;
+      });
+      setChartData({
+        labels: currentWeekDays.map((day) => format(day, "EEE")),
+        datasets: [
+          {
+            label: "Weekly Sales",
+            data: mappedSalesData,
+            borderColor: "rgba(75, 192, 192, 1)",
+            backgroundColor: "rgba(75, 192, 192, 0.2)",
+            borderWidth: 2,
+            fill: false,
+            tension: 0.4,
+            pointRadius: mappedSalesData.map((sales) => (sales ? 3 : 0)),
+            pointBackgroundColor: "rgba(75, 192, 192, 1)",
+          },
+        ],
+      });
+    } else if (activeData === "monthly" && monthlyData) {
+      const currentYearMonths = eachMonthOfInterval({
+        start: startOfYear(new Date()),
+        end: endOfYear(new Date()),
+      });
+
+      const mappedMonthlySalesData = currentYearMonths.map((month) => {
+        const saleForMonth = monthlyData.monthlySales.find(
+          (sale) =>
+            format(new Date(sale.date), "yyyy-MM") === format(month, "yyyy-MM")
+        );
+        return saleForMonth ? saleForMonth.totalSales : 0;
+      });
+      setChartData({
+        labels: currentYearMonths.map((month) => format(month, "MMM")),
+        datasets: [
+          {
+            label: "Monthly Sales",
+            data: mappedMonthlySalesData,
+            borderColor: "rgba(75, 192, 192, 1)",
+            backgroundColor: "rgba(75, 192, 192, 0.2)",
+            borderWidth: 2,
+            fill: false,
+            tension: 0.4,
+            pointRadius: mappedMonthlySalesData.map((sales) => (sales ? 3 : 0)),
+            pointBackgroundColor: "rgba(75, 192, 192, 1)",
+          },
+        ],
+      });
+    }
+
+    const showDropdownHandler = (e) => {
+      const dropdownEl = document.getElementById("dropdown");
+      if (!dropdownEl.contains(e.target)) {
+        setShow(false);
+      } else {
+        setShow(true);
+      }
+    };
+
+    window.addEventListener("click", showDropdownHandler);
+    return () => window.removeEventListener("click", showDropdownHandler);
+  }, [activeData, weeklyData, monthlyData]);
+
+  return (
+    <div className="max-h-96 mt-16 mb-10">
+      <div className="max-w-[760px] w-full bg-slate-200 relative">
+        <div
+          className="w-1/3 bg-white flex flex-col gap-3 py-5 absolute right-1 -top-5"
+          id="dropdown"
+          onClick={() => setShow(!show)}
+        >
+          <p className="px-5 text-[17px] flex items-center gap-2 cursor-default text-[#929292]">
+            <span>
+              {activeData === "weekly"
+                ? "Weekly Sales"
+                : activeData === "monthly"
+                ? "Monthly Sales"
+                : "Sales"}
+            </span>
+            <span className="mt-1">
+              <DropdownIcon show={show} />
+            </span>
+          </p>
+          {show && (
+            <Fragment>
+              <button
+                className={`text-left px-5 py-2 ${
+                  activeData === "weekly" ? "bg-[#23cece45]" : ""
+                }`}
+                onClick={() => {
+                  setActiveData("weekly");
+                  setShow(false);
+                }}
+                disabled={activeData === "weekly"}
+              >
+                Weekly Sales
+              </button>
+              <button
+                className={`text-left px-5 py-2 ${
+                  activeData === "monthly" ? "bg-[#23cece45]" : ""
+                }`}
+                onClick={() => {
+                  setActiveData("monthly");
+                  setShow(false);
+                }}
+                disabled={activeData === "monthly"}
+              >
+                Monthly Sales
+              </button>
+            </Fragment>
+          )}
+        </div>
+      </div>
+
+      {chartData && (
+        <Line
+          data={chartData}
+          options={{
+            responsive: true,
+            plugins: {
+              legend: {
+                position: "top",
+              },
+              title: {
+                display: true,
+              },
+            },
+            scales: {
+              x: {
+                title: {
+                  display: true,
+                  text: "Order Time",
+                },
+              },
+              y: {
+                title: {
+                  display: true,
+                  text: "Sales Amount",
+                },
+              },
+            },
+          }}
+        />
+      )}
+    </div>
+  );
+};
+
+export default Sales;
--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,18 +1,55 @@
 "use client";
 
 import TitleMenu from "../components/title-menu";
-import { useLocation } from "react-router-dom"
+import { useLocation } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import Sales from "../components/Sales";
+import { useEffect, useState } from "react";
+import { DAILY_SALES } from "../query";
 
 export default function Home() {
-  // const pathname = usePathname();
-  // const path = pathname.slice(1);
   const location = useLocation();
   const pathname = location?.pathname;
-  const path = pathname.slice(1)
+  const path = pathname.slice(1);
 
+  const [dailySales, setDailySales] = useState({
+    totalOrders: 0,
+    totalSalesAmount: 0,
+  });
+  const { data } = useQuery(DAILY_SALES);
+
+  useEffect(() => {
+    if (data?.dailySales)
+      setDailySales(data.dailySales);
+  }, [data]);
   return (
-    <div className="px-5">
+    <div className="px-5 pb-10 overflow-y-auto h-screen">
       <TitleMenu pageType="pages" pageName={path} />
+      <div className="mt-12">
+        <div className="bg-white shadow-lg w-[280px] max-[320px]:w-full p-3 flex items-center gap-2 justify-between rounded-lg">
+          <div className="flex flex-col gap-1">
+            <span className="text-gray-400 text-lg font-semibold">Sales</span>
+            <span className="font-bold text-3xl text-gray-800">${dailySales.sales}</span>
+          </div>
+          <div className="bg-linear-gradient py-3 px-3 rounded-lg">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={2}
+              stroke="currentColor"
+              className="w-6 stroke-white"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+              />
+            </svg>
+          </div>
+        </div>
+        <Sales />
+      </div>
     </div>
   );
 }
